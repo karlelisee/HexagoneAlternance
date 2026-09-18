83b27c68f0734aa0215d59cc5e8ab1a76133a72b
Karl-Elisee KOFFI <karlelisee777@gmail.com>
2026-09-18 09:51:19 +0200

    correction avatar KK

--- scrap.py
+++ scrap.py
@@ -918,7 +918,7 @@ def generer_html(offres: list[dict]) -> str:
     </div>
 
     <div class="topbar-right">
-      <div class="avatar" title="Compte (démo)">KK</div>
+      # <div class="avatar" title="Compte (démo)">KK</div>
     </div>
   </div>
 </div>
@@ -928,7 +928,7 @@ def generer_html(offres: list[dict]) -> str:
 
     <div class="panel">
       <div class="profil">
-        <div class="avatar">KK</div>
+        # <div class="avatar">KK</div>
         <div>
           <div class="profil-nom">Étudiant Hexagone</div>
           <div class="profil-sous">Versailles · M1</div>
